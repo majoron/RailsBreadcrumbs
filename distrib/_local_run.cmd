9db075538f4633f571e74092a5b72888d6a3a1df
Artem Rufanov <arufanov@gmail.com>
2011-12-27 23:39:54 +0400

    Hardcoded gemspec name has been changed on ant properties

--- distrib/_local_run.cmd
+++ distrib/_local_run.cmd
@@ -11,6 +11,8 @@ SET PATH=%PATH%;C:/Binutils/rubygems-1.3.5/bin
 SET PATH=C:/Binutils/apache-ant-1.7.0/bin/;%PATH%
 SET PATH=D:/Binutils/apache-ant-1.7.0/bin/;%PATH%
 
+Rem: set java home
+SET JAVA_HOME=C:/Progra~1/Java/jre6
 
 echo on
 Rem: set enviroment and execute ant script
