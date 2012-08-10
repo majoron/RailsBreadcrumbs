73accdf3d11b01af34384919ab94d36625bb916a
arufanov <arufanov@gmail.com>
2012-08-11 00:31:00 +0400

    Path fixes

--- distrib/_local_release.cmd
+++ distrib/_local_release.cmd
@@ -5,12 +5,8 @@ rem
 
 Rem: set path 
 SET PATH=
-SET PATH=%PATH%;D:/Binutils/Ruby192/bin
-SET PATH=%PATH%;C:/Binutils/Ruby192/bin
-SET PATH=%PATH%;D:/Binutils/Ruby-186-27/bin
-SET PATH=%PATH%;C:/Binutils/Ruby-186-27/bin
-SET PATH=%PATH%;D:/Binutils/rubygems-1.3.5/bin
-SET PATH=%PATH%;C:/Binutils/rubygems-1.3.5/bin
+SET PATH=%PATH%;D:/Binutils/Ruby193/bin
+SET PATH=%PATH%;C:/Binutils/Ruby193/bin
 SET PATH=C:/Progra~1/Git/bin;%PATH%
 
 Rem: set enviroment and release gem
